9284816caf2a7e38f5f634a21ac9fbc559b9633e
AkashDeep Singh Mehra <as.mehra007@gmail.com>
2018-09-24 19:04:42 +0530

    Add files via upload

---
+++ Models/RequestData.cs
@@ -0,0 +1,17 @@
+﻿using ChatBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper.Models
+{
+   public class RequestData
+    {
+        public string matchParameter { get; set; }
+        public int sequence { get; set; }
+        public string  requestToken { get; set; }
+        public List<Patient> patList { get; set; }
+    }
+}
